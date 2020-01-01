f6593f81f873384a75d3cfe05be814cc90deb17a
Akronborn <freewillandjustice@gmail.com>
2020-01-01 00:58:42 -0500

    restored app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const config = require('config');
-// const dbConfig = config.get('Mysql.dbConfig');
-const dbConfig = require('../config/db.config');
+const dbConfig = config.get('Mysql.dbConfig');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,12 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-const pool = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+const pool = mysql.createPool(dbConfig);
 
 app.get('/', function (req, res) {
   pool.query('SELECT COUNT(*) AS count FROM users', function (
@@ -34,9 +29,6 @@ app.get('/', function (req, res) {
 
 });
 
-
-
-// try
 app.post('/signup', (req, res) => {
   let data = {
     email: req.body.email,
@@ -44,8 +36,6 @@ app.post('/signup', (req, res) => {
     native: req.body.native,
     level: req.body.level
   };
-
-  // let submission = `INSERT INTO users(email, target, native, level) VALUES (?, ?, ?, ?)`;
 
   pool.query('INSERT INTO users SET ?', data, function (err, result) {
     if (err) throw err;
@@ -58,5 +48,3 @@ app.post('/signup', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}!`);
 });
-
-module.exports = pool;
